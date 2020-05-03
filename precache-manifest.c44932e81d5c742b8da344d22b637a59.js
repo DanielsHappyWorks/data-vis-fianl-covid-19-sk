812477237c2bfd04635f25b1fb3b83a0cd5f9c71
Daniel Foth <daniel.foth@ericsson.com>
2020-05-03 18:52:35 +0100

    Updates

---
+++ precache-manifest.c44932e81d5c742b8da344d22b637a59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60fc710dd348d71d57424b34dce9f025",
+    "url": "/covid_19/index.html"
+  },
+  {
+    "revision": "88a0e1010eaeec92b610",
+    "url": "/covid_19/static/css/2.d0fd96af.chunk.css"
+  },
+  {
+    "revision": "7a399b8460749501274a",
+    "url": "/covid_19/static/css/main.c2d19781.chunk.css"
+  },
+  {
+    "revision": "88a0e1010eaeec92b610",
+    "url": "/covid_19/static/js/2.9b8495f4.chunk.js"
+  },
+  {
+    "revision": "7a399b8460749501274a",
+    "url": "/covid_19/static/js/main.cd6c432a.chunk.js"
+  },
+  {
+    "revision": "7b487fde57e357d9a9fd",
+    "url": "/covid_19/static/js/runtime~main.d4bbeef2.js"
+  }
+]);
